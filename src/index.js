d635ad52827d76cb77d14bafbfbdd04678ce6ddb
JC TINIO <s20016@std.it-college.ac.jp>
2022-04-18 17:02:27 +0900

    Add: Ground parallax

--- src/index.js
+++ src/index.js
@@ -6,25 +6,30 @@ class MyGame extends Phaser.Scene {
   }
 
   preload() {
-    this.load.audio('bgm', ['./src/assets/bgm.mp3']);
+    // this.load.audio('bgm', ['./src/assets/bgm.mp3']);
+    this.load.image('asphalt', './src/assets/asphalt.png');
 
     this.load.spritesheet('rebel', './src/assets/rebel.png', {
       frameWidth: 500,
       frameHeight: 500
     });
 
-    this.load.spritesheet('road', './src/assets/road.png', {
-      frameWidth: 500,
-      frameHeight: 500
-    })
+    // this.load.spritesheet('road', './src/assets/road.png', {
+    //   frameWidth: 500,
+    //   frameHeight: 500
+    // })
 
-    this.load.spritesheet('bg', './src/assets/bg.png', {
-      frameWidth: 500,
-      frameHeight: 500
-    })
+    // this.load.spritesheet('bg', './src/assets/bg.png', {
+    //   frameWidth: 500,
+    //   frameHeight: 500
+    // })
   }
 
   create() {
+
+    // Key input
+    this.cursorKeys = this.input.keyboard.createCursorKeys();
+
     // BGM
     // this.bgm = this.sound.add('bgm');
     // let musicConfig = {
@@ -41,13 +46,19 @@ class MyGame extends Phaser.Scene {
 
     // Rebel
     this.rebel = this.add.sprite(300, 200, 'rebel').setDepth(5);
+
     this.anims.create({
-      key: 'rebel_anim',
-      frames: this.anims.generateFrameNumbers('rebel',
-        {start: 0, end: 2}),
-      frameRate: 10,
+      key: 'move_anim',
+      frames: this.anims.generateFrameNumbers('rebel', {start: 1, end: 3}),
+      frameRate: 18,
       repeat: -1
-    }); 
+    });
+
+    this.anims.create({
+      key: 'idle',
+      frames: this.anims.generateFrameNumbers('rebel', {start: 0, end: 0}),
+      frameRate: 1,
+    })
 
     // this.anims.create({
     //   key: 'kato_anim',
@@ -62,6 +73,10 @@ class MyGame extends Phaser.Scene {
     // this.road2 = this.add.sprite(500, 360, 'road').setDepth(4);
     // this.road3 = this.add.sprite(1000, 360, 'road').setDepth(4);
 
+    // this.asphalt = this.add.image(50, 450, 'asphalt').setDepth(4);
+    this.asphalt = this.add.tileSprite(0, 400, game.config.width, game.config.height, 'asphalt')
+      .setOrigin(0, 0).setScrollFactor(0);
+
     // this.anims.create({
     //   key: 'road_anim',
     //   frames: this.anims.generateFrameNumbers('road'),
@@ -74,20 +89,36 @@ class MyGame extends Phaser.Scene {
     // this.bg = this.add.image(700, 240, 'bg').setDepth(0);
     // this.bg = this.add.image(1000, 240, 'bg').setDepth(0);
 
-    this.rebel.play('rebel_anim');
+    // this.rebel.play('move_anim');
     // this.road.play('road_anim');
     // this.road2.play('road_anim');
     // this.road3.play('road_anim');
+    // this.cameras.main.startFollow(this.rebel);
+  }
+
+  update() {
+    // this.cameras.main.startFollow(this.rebel);
+    // this.asphalt.tilePositionX = this.cameras.x * -1;
+
+    if (this.cursorKeys.up.isDown) {
+      this.rebel.play('move_anim', true);
+      this.asphalt.tilePositionX += 8;
+      // this.cameras.main.startFollow(this.rebel);
+      // this.asphalt.tilePositionX = this.cameras.x * .00000000000000002;
+      // this.cameras.main.startFollow(this.rebel);
+      // this.asphalt.tilePositionX = this.cameras.main.scrollX * .3;
+      // this.rebel.anims.msPerFrame = 20;
+    } else {
+      this.rebel.play('idle', true);
+    }
   }
 }
 
-const config = {
+const game = new Phaser.Game({
   type: Phaser.AUTO,
   parent: 'phaser-example',
   width: 1200,
   height: 500,
-  transparent: true,
+  backgroundColor: '#99d6f0',
   scene: MyGame
-};
-
-const game = new Phaser.Game(config);
+});
