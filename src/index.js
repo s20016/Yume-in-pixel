7dcdbe3430b2e388d4922534fd2fff3cbbe22bad
JC TINIO <s20016@std.it-college.ac.jp>
2022-04-18 14:41:42 +0900

    Add: BGM

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ class MyGame extends Phaser.Scene {
   }
 
   preload() {
-    this.load.spritesheet('rebel', './src/assets/dream.png', {
+    this.load.audio('bgm', ['./src/assets/bgm.mp3']);
+
+    this.load.spritesheet('rebel', './src/assets/rebel.png', {
       frameWidth: 500,
       frameHeight: 500
     });
@@ -23,37 +25,59 @@ class MyGame extends Phaser.Scene {
   }
 
   create() {
+    // BGM
+    // this.bgm = this.sound.add('bgm');
+    // let musicConfig = {
+    //   mute: false,
+    //   volume: 1,
+    //   rate: 1, 
+    //   detune: 0,
+    //   seek: 90,
+    //   loop: true,
+    //   delay: 0
+    // };
+
+    // this.bgm.play();
+
     // Rebel
     this.rebel = this.add.sprite(300, 200, 'rebel').setDepth(5);
     this.anims.create({
       key: 'rebel_anim',
       frames: this.anims.generateFrameNumbers('rebel',
-        {start: 3, end: 4}),
+        {start: 0, end: 2}),
       frameRate: 10,
       repeat: -1
     }); 
+
+    // this.anims.create({
+    //   key: 'kato_anim',
+    //   frames: this.anims.generateFrameNumbers('rebel',
+    //     {start: 3, end: 4}),
+    //   frameRate: 10,
+    //   repeat: -1
+    // }); 
     
     // Road
-    this.road = this.add.sprite(100, 360, 'road').setDepth(4);
-    this.road2 = this.add.sprite(500, 360, 'road').setDepth(4);
-    this.road3 = this.add.sprite(1000, 360, 'road').setDepth(4);
+    // this.road = this.add.sprite(100, 360, 'road').setDepth(4);
+    // this.road2 = this.add.sprite(500, 360, 'road').setDepth(4);
+    // this.road3 = this.add.sprite(1000, 360, 'road').setDepth(4);
 
-    this.anims.create({
-      key: 'road_anim',
-      frames: this.anims.generateFrameNumbers('road'),
-      frameRate: 5,
-      repeat: -1
-    });
+    // this.anims.create({
+    //   key: 'road_anim',
+    //   frames: this.anims.generateFrameNumbers('road'),
+    //   frameRate: 5,
+    //   repeat: -1
+    // });
 
     // BG
-    this.bg = this.add.image(250, 240, 'bg').setDepth(0);
-    this.bg = this.add.image(700, 240, 'bg').setDepth(0);
-    this.bg = this.add.image(1000, 240, 'bg').setDepth(0);
+    // this.bg = this.add.image(250, 240, 'bg').setDepth(0);
+    // this.bg = this.add.image(700, 240, 'bg').setDepth(0);
+    // this.bg = this.add.image(1000, 240, 'bg').setDepth(0);
 
     this.rebel.play('rebel_anim');
-    this.road.play('road_anim');
-    this.road2.play('road_anim');
-    this.road3.play('road_anim');
+    // this.road.play('road_anim');
+    // this.road2.play('road_anim');
+    // this.road3.play('road_anim');
   }
 }
 
