904d06a90e11dae0a42b2e40543a2a760c880f85
JC TINIO <s20016@std.it-college.ac.jp>
2022-04-21 14:34:17 +0900

    Add: Load

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ class MyGame extends Phaser.Scene {
   }
 
   preload() {
-    this.load.audio('bgm', [ './src/assets/bgm.ogg', './src/assets/bgm.mp3' ]);
+    this.load.audio('bgm', ['./src/assets/bgm.ogg', './src/assets/bgm.mp3']);
     this.load.image('asphalt', './src/assets/asphalt.png');
     this.load.image('beach', './src/assets/beach.png');
     this.load.image('ocean', './src/assets/ocean.png');
@@ -17,54 +17,121 @@ class MyGame extends Phaser.Scene {
       frameWidth: 500,
       frameHeight: 500
     });
+
+    // Loader
+    var progressBar = this.add.graphics();
+    var progressBox = this.add.graphics();
+    progressBox.fillStyle(0x222222, 0.8);
+    progressBox.fillRect(440, 270, 320, 50);
+
+    var width = this.cameras.main.width;
+    var height = this.cameras.main.height;
+    var loadingText = this.make.text({
+      x: width / 2,
+      y: height / 2,
+      text: 'Loading...',
+      style: {
+        font: '20px monospace',
+        fill: '#ffffff'
+      }
+    });
+    loadingText.setOrigin(0.5, 0.5);
+
+    var percentText = this.make.text({
+      x: width / 2,
+      y: height / 2 + 45,
+      text: '0%',
+      style: {
+        font: '18px monospace',
+        fill: '#ffffff'
+      }
+    });
+    percentText.setOrigin(0.5, 0.5);
+
+    var assetText = this.make.text({
+      x: width / 2,
+      y: height / 2 + 85,
+      text: '',
+      style: {
+        font: '18px monospace',
+        fill: '#ffffff'
+      }
+    });
+    assetText.setOrigin(0.5, 0.5);
+
+    this.load.on('progress', function (value) {
+      console.log(value);
+      progressBar.clear();
+      progressBar.fillStyle(0xffffff, 1);
+      progressBar.fillRect(450, 280, 300 * value, 30);
+      percentText.setText(parseInt(value * 100) + '%');
+    });
+
+    this.load.on('fileprogress', function (file) {
+      console.log(file.src);
+      assetText.setText('Loading asset: ' + file.key);
+    });
+    this.load.on('complete', function () {
+      console.log('complete');
+      progressBar.destroy();
+      progressBox.destroy();
+      loadingText.destroy();
+      percentText.destroy();
+      assetText.destroy();
+    });
   }
 
 
 
   create() {
+    // BGM
     this.music = this.sound.add('bgm');
-    // this.cache.isSoundDecoded("bgm");
     this.music.play();
 
+    // Control
     this.cursorKeys = this.input.keyboard.createCursorKeys();
+
+    // Main image
     this.rebel = this.add.sprite(300, 200, 'rebel').setDepth(5);
 
+    // Animations
     this.anims.create({
       key: 'idle',
-      frames: this.anims.generateFrameNumbers('rebel', {start: 0, end: 0}),
+      frames: this.anims.generateFrameNumbers('rebel', { start: 0, end: 0 }),
       frameRate: 1,
     })
 
     this.anims.create({
       key: 'move_anim',
-      frames: this.anims.generateFrameNumbers('rebel', {start: 3, end: 5}),
+      frames: this.anims.generateFrameNumbers('rebel', { start: 3, end: 5 }),
       frameRate: 12,
       repeat: -1,
     });
 
     this.anims.create({
       key: 'back_anim',
-      frames: this.anims.generateFrameNumbers('rebel', {start: 1, end: 2}),
+      frames: this.anims.generateFrameNumbers('rebel', { start: 1, end: 2 }),
       frameRate: 3,
       repeat: -1,
     });
 
     this.anims.create({
       key: 'kato_anim',
-      frames: this.anims.generateFrameNumbers('rebel', {start: 6, end: 7}),
+      frames: this.anims.generateFrameNumbers('rebel', { start: 6, end: 7 }),
       frameRate: 10,
       repeat: -1
-    }); 
+    });
 
+    // Parallex images
     this.asphalt = this.add.tileSprite(0, 400, game.config.width, game.config.height, 'asphalt')
       .setOrigin(0, 0).setScrollFactor(0).setDepth(4);
-    
+
     this.beach = this.add.tileSprite(0, 355, game.config.width, game.config.height, 'beach')
       .setOrigin(0, 0).setScrollFactor(0).setDepth(3);
 
     this.ocean = this.add.tileSprite(0, 325, game.config.width, game.config.height, 'ocean')
       .setOrigin(0, 0).setScrollFactor(0).setDepth(2);
-    
+
     this.cloud = this.add.tileSprite(0, 0, game.config.width, game.config.height, 'cloud')
       .setOrigin(0, 0).setScrollFactor(0).setDepth(1);
 
@@ -82,8 +149,8 @@ class MyGame extends Phaser.Scene {
         this.forwardMove();
       }
     } else if (this.cursorKeys.down.isDown) {
-        this.rebel.play('back_anim', true);
-        this.backwardMove();
+      this.rebel.play('back_anim', true);
+      this.backwardMove();
     } else {
       this.rebel.play('idle', true);
       // this.add.text(game.config.width - 230, 20, 'Press Up or Down. Shift for surprise').setDepth(7);
@@ -113,6 +180,5 @@ const game = new Phaser.Game({
   parent: 'phaser-example',
   width: 1200,
   height: 500,
-  backgroundColor: '#88d0ef',
   scene: MyGame
 });
